dac6ceb72a77cd4858356b38c904eda75538ea03
Bartosz <barzak200@gmail.com>
2021-09-16 18:16:46 +0200

    Playing with colors and main heading

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div>
-      <MainHeader content="Mood Tracker" />
+      <MainHeader/>
 
       <NewMood onAddNewMood={addNewMoodHandler} />
 
--- src/components/UI/MainHeader.js
+++ src/components/UI/MainHeader.js
@@ -3,7 +3,10 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header>
-      <h1 className={classes['heading-primary']}>{props.content}</h1>
+      <h1 className={classes['heading-primary']}>
+        <div className={classes['heading-primary--first-word']}>Mood</div>
+        <div className={classes['heading-primary--second-word']}>TRACKER</div>
+      </h1>
     </header>
   );
 };
